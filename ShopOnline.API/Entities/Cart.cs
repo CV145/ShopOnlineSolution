f0514606d15a69a3d087bf251cef96ec47bec520
Carlos <carlosvaleriano45@yahoo.com>
2023-01-02 16:28:35 -0600

    Add project files.

---
+++ ShopOnline.API/Entities/Cart.cs
@@ -0,0 +1,9 @@
+﻿namespace ShopOnline.API.Entities
+{
+    //Class that represents Cart entity model
+    public class Cart
+    {
+        public int Id { get; set; }
+        public string UserId { get; set; }
+    }
+}
---
+++ ShopOnline.API/Entities/CartItem.cs
@@ -0,0 +1,11 @@
+﻿namespace ShopOnline.API.Entities
+{
+    public class CartItem
+    {
+        public int Id { get; set; }
+        //Foreign key - point to cart containing this item
+        public int CartId { get; set; }
+        public int ProductId { get; set; }
+        public int Qty { get; set; }
+    }
+}
